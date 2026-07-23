f4587a1e78a043b521860d6a507cdffa4cc8ee5e
HiruniKaveesha49 <desilva-im23063@stu.kln.ac.lk>
2026-07-23 17:28:06 +0530

    Implement game logic in app.js for number guessing functionality

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("Hello");
+function checkValue() {
+    let inputNumber = document.getElementById("inputNumber").value
+    console.log(inputNumber);
+
+    let num = Math.random() * 10 + 1
+
+    let randomNumber = Math.floor(num)
+    console.log(randomNumber);
+
+    if (inputNumber == randomNumber) {
+        Swal.fire({
+            title: "Congratulation!🤩...You are Win",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(/images/trees.png)",
+            backdrop: `
+                       
+                        rgba(64, 0, 123, 0.4)
+                        url("images/win.gif\")
+                     `
+        });
+
+    } else {
+        Swal.fire({
+            title: "You are fail😪...Try again",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(/images/trees.png)",
+            backdrop: `
+                       
+                        rgba(64, 0, 123, 0.4)
+                        url("images/nyan-cat.gif\")
+                        left center / 300px auto
+                        no-repeat
+                        `
+        });
+
+    }
+}
+
+function resetButton() {
+    document.getElementById("inputNumber").value = ""
+}
