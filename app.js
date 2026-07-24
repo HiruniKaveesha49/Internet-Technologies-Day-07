a99a3f9b37837b616f026dc19c071d299974b5b5
HiruniKaveesha49 <desilva-im23063@stu.kln.ac.lk>
2026-07-25 01:46:35 +0530

    Refactor app.js and index.html to remove old code and implement product fetching functionality

--- app.js
+++ app.js
@@ -149,13 +149,81 @@
 
 // console.log(employeeSalaryItem);
 
-const studentList = [
-    {id:"001", name:"sunil"},
-    {id:"002", name:"wimal"},
-    {id:"003", name:"kamal"},
-    {id:"004", name:"ranil"}
-]
+// const studentList = [
+//     {id:"001", name:"sunil"},
+//     {id:"002", name:"wimal"},
+//     {id:"003", name:"kamal"},
+//     {id:"004", name:"ranil"}
+// ]
 
-let students = studentList.find(student => student.id==="003")
+// let students = studentList.find(student => student.id==="003")
 
-console.log(students.name);
+// console.log(students.name);
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+// .then(res=>res.json())
+// .then(data=>{
+
+//     console.log(data);
+
+//     let tblTodoList = document.getElementById("tblTodoList")
+
+//     let body = "";
+
+//     data.forEach(element => {
+//         body +=`
+//             <tr>
+//                 <td>${element.id}</td>
+//                 <td>${element.title}</td>
+//                 <td>${element.userId}</td>
+//                 <td>${element.completed}</td>
+//             </tr>
+
+//         `
+        
+//     });
+//     tblTodoList.innerHTML=body;
+// });
+
+
+
+
+fetch("https://fakestoreapi.com/products/")
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+    
+
+    let productContainner = document.getElementById("productContainner")
+    
+    let body=""
+
+    data.forEach(element => {
+        
+        body+=
+        `<div class="col">
+                    <div class="card shadow-sm h-100"> 
+                        <div><img src="${element.image}" alt="" class="card-img-top object-fit-cover"></div>
+                            <div class="card-body">
+                            <h1 class="card-title">${element.title}</h1>
+                            <p class="card-text">${element.description}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class=""> 
+                                    <button type="button"
+                                        class="btn btn-sm btn-success">Buy Now LKR ${element.price}$</button>
+                                    </div> 
+                                    <small
+                                        class="text-body-secondary">${element.rating.count}/${element.rating.rate}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `
+        });
+
+        productContainner.innerHTML = body;
+        
+
+});
